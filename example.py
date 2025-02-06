a0a4adc69de9cee809988413124fdc1abb5ac545
Mr. Coder <den.takoy@gmail.com>
2025-02-06 13:42:42 +0300

    refactoring and cleaning

--- example.py
+++ example.py
@@ -1,14 +1,17 @@
-from modules.tg_miniapp_auth import validate_init_data
+from urllib.parse               import parse_qs
+
+from modules.tg_miniapp_auth    import validate_init_data, NotAuthorized
 
 
-bot_id      = "1234567890"
-init_data   = "user=%7B..."
+bot_id              = 1234567890
+init_data_string    = "user=%7B%..."
 
 
 if __name__ == "__main__":
-    is_valid = validate_init_data(init_data, bot_id)
+    init_data = parse_qs(init_data_string, keep_blank_values = True)
 
-    if is_valid:
-        print("Authorized")
-    else:
-        print("Not authorized")
+    try:
+        validate_init_data(init_data, bot_id)
+        print('Authorized')
+    except NotAuthorized as e:
+        print('Not authorized: ' + str(e))
--- modules/tg_miniapp_auth.py
+++ modules/tg_miniapp_auth.py
@@ -1,5 +1,8 @@
+import typing
+import time
 import base64
-from urllib.parse import parse_qs, unquote
+
+from urllib.parse       import unquote
 
 from nacl.signing       import VerifyKey
 from nacl.exceptions    import BadSignatureError
@@ -7,50 +10,49 @@ from nacl.exceptions    import BadSignatureError
 
 # Telegram Public Keys
 PUBLIC_KEYS = {
-    "production":   bytes.fromhex("e7bf03a2fa4602af4580703d88dda5bb59f32ed8b02a56c187fe7d34caed242d"),
-    "test":         bytes.fromhex("40055058a4ee38156a06562e52eece92a771bcd8346a8c4615cb7376eddf72ec")
+    'production':   bytes.fromhex('e7bf03a2fa4602af4580703d88dda5bb59f32ed8b02a56c187fe7d34caed242d'),
+    'test':         bytes.fromhex('40055058a4ee38156a06562e52eece92a771bcd8346a8c4615cb7376eddf72ec')
 }
-
-
-class NoSignatureInInitData(Exception):
-    pass
 
 
 class NotAuthorized(Exception):
     pass
 
 
-def validate_init_data( init_data:      str,
-                        bot_id:         str,
-                        environment:    str     = "production",
-                        exception_mode: bool    = False
-) -> bool:
-    params = parse_qs(init_data, keep_blank_values = True)
+def init_data_to_string(init_data: dict, bot_id: int):
+    check_string = "\n" . join(f"{key}={unquote(value[0])}"
+        for key, value in sorted(init_data.items()))
 
-    # Extract signature and hash, and clean params
-    signature = params.pop("signature", [ None ])[0]
-    params.pop("hash", None)
+    return f"{bot_id}:WebAppData\n{check_string}".encode()
 
+
+def validate_init_data(
+        init_data:      dict[typing.AnyStr, list[typing.AnyStr]],
+        bot_id:         int,
+        expires_in:     int = 3600,
+        environment:    str = 'production',
+):
+    auth_date_string = init_data.get('auth_date', [ None ])[0]
+    if not auth_date_string or not auth_date_string.isdigit():
+        raise NotAuthorized('No auth_date in initData')
+
+    if int(time.time()) - int(auth_date_string) > expires_in:
+        raise NotAuthorized('initData auth_time expired')
+
+    # Get and remove signature from initData
+    signature = init_data.pop('signature', [ None ])[0]
     if not signature:
-       raise NoSignatureInInitData
+        raise NotAuthorized('No signature in initData')
 
-    # Ensure Base64 padding for signature
-    signature       += "=" * ((4 - len(signature) % 4) % 4)
-    signature_bytes = base64.urlsafe_b64decode(signature)
+    # remove hash from initData
+    init_data.pop('hash', None)
 
-    # Format key-value pairs
-    check_string = "\n" . join(f"{k}={unquote(v[0])}"
-        for k, v in sorted(params.items()))
-
-    data_string = f"{bot_id}:WebAppData\n{check_string}"
-    verify_key  = VerifyKey(PUBLIC_KEYS[environment])
+    verify_key          = VerifyKey(PUBLIC_KEYS[environment])
+    init_data_string    = init_data_to_string(init_data, bot_id)
+    signature           += '=' * ((4 - len(signature) % 4) % 4) # base64 padding
+    signature_bytes     = base64.urlsafe_b64decode(signature)
 
     try:
-        verify_key.verify(data_string.encode(), signature_bytes)
-        return True
-
+        verify_key.verify(init_data_string, signature_bytes)
     except BadSignatureError:
-        if exception_mode:
-            raise NotAuthorized
-
-        return False
+        raise NotAuthorized('initData signature is not valid')
